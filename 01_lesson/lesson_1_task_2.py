912edb58edfb232107737488796baa49cba35227
temur1980/test-repo <temur-bil@rambler.ru>
2025-12-24 23:15:29 +0300

    обавлены файлы домашней работы к уроку 1

---
+++ 01_lesson/lesson_1_task_2.py
@@ -0,0 +1,3 @@
+my_age = 45
+my_age_age_after_3_years = 48
+print(f"Мне {my_age} лет, через три года будет {my_age_age_after_3_years}")
---
+++ 01_lesson/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = "Темур"
+last_name = "Билялов"
+print(f"Вас зовут: {last_name} {first_name}")
